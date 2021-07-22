477f4ca92ff34137e4c924333ad2c5ded7f1f9bb
SnehalBhavana <87724168+SnehalBhavana@users.noreply.github.com>
2021-07-22 20:15:33 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,15 +1,94 @@
 <?php
-    ob_start();
-    session_start();
 
-    $timezone = date_default_timezone_set("Europe/London");
+ 
+// Error Reporting Turn On
 
-    $con = mysqli_connect("database.c100jh4vmti7.us-east-1.rds.amazonaws.com", "admin", "admin123", "slotifyfree");
+ 
+ini_set('error_reporting', E_ALL);
 
-    if(mysqli_connect_errno()) {
-    	echo "Failed to connect:" . mysqli_connect_errno();
-    }
+ 
+ 
 
+ 
+// Setting up the time zone
 
+ 
+date_default_timezone_set('Asia/Kolkata');
 
-?>
+ 
+ 
+
+ 
+// Host Name
+
+ 
+$dbhost = 'database.c100jh4vmti7.us-east-1.rds.amazonaws.com';
+
+ 
+ 
+
+ 
+// Database Name
+
+ 
+$dbname = 'slotifyfree';
+
+ 
+ 
+
+ 
+// Database Username
+
+ 
+$dbuser = 'admin';
+
+ 
+ 
+
+ 
+// Database Password
+
+ 
+$dbpass = 'admin123';
+
+ 
+ 
+
+ 
+// Defining base url
+
+ 
+define("BASE_URL", "http://slotify-env.eba-ddmint8v.us-east-1.elasticbeanstalk.com/register.php");
+
+ 
+ 
+
+ 
+// Getting Admin url
+
+ 
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+ 
+ 
+
+ 
+try {
+
+ 
+$con = new con("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+
+ 
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ 
+}
+
+ 
+catch( PDOException $exception ) {
+
+ 
+echo "Connection error :" . $exception->getMessage();
+
+ 
+}
